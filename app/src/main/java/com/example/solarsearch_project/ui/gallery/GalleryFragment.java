2ac9780dfb95883cdd13adff958d8c1d01ba9d4f
blocho <blocho@bbcag.ch>
2023-07-05 12:50:57 +0200

    change search page

--- app/src/main/java/com/example/solarsearch_project/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/solarsearch_project/ui/gallery/GalleryFragment.java
@@ -1,20 +1,26 @@
 package com.example.solarsearch_project.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.solarsearch_project.R;
 import com.example.solarsearch_project.databinding.FragmentGalleryBinding;
+import com.example.solarsearch_project.models.Element;
 
 public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
+    private static final String Solar_API_ID = "https://api.le-systeme-solaire.net/rest.php/bodies?data=id";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,7 +31,13 @@ public class GalleryFragment extends Fragment {
         View root = binding.getRoot();
 
 
+
+
+
         return root;
+
+
+
     }
 
     @Override
@@ -33,4 +45,6 @@ public class GalleryFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+
 }
