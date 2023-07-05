2b76b00245d3c7cd276c2573ea3ef52494076764
blocho <blocho@bbcag.ch>
2023-07-05 09:30:21 +0200

    change of homeFragment

--- app/src/main/java/com/example/solarsearch_project/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/solarsearch_project/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.solarsearch_project.ui.home;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,10 +13,22 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.solarsearch_project.databinding.FragmentHomeBinding;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+
+    private static final String NASA_API = "https://api.nasa.gov/planetary/apod?api_key=n9YNX94gAtAvKEijfUzDcfeA8kBVdKnoOLYiAn20";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +38,13 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        String nasaImgUrl = fetchNasaImg();
+        if(nasaImgUrl != null) {
+            showNasaImg(nasaImgUrl);
+        }
 
         return root;
     }
@@ -33,6 +54,60 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    public String fetchNasaImg() {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(NASA_API)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            String jsonString = response.body().string();
+
+            JSONObject jsonObject = new JSONObject(jsonString);
+            return jsonObject.getString("url");
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void showNasaImg(String url) {
+        ImageView pictureoftheday = binding.pictureoftheday;
+        Picasso.get().load(url).into(pictureoftheday);
+    }
+
+//    public void NasaIMG() {
+//        String url = "";
+//
+//        OkHttpClient client = new OkHttpClient();
+//
+//        Request request = new Request.Builder()
+//                .url(NASA_API)
+//                .build();
+//
+//        try {
+//            Response response = client.newCall(request).execute();
+//            String jsonString = response.body().string();
+//
+//            JSONObject jsonObject = new JSONObject(jsonString);
+//            url = jsonObject.getString("url");
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//
+////        ImageView imageView = findViewById(R.id.pictureoftheday);
+////        Picasso.get().load(url).into(imageView);
+//    }
 
 
 }
