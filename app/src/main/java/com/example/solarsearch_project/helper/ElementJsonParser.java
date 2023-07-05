1894a9af8f1c0476988dd9d2e8c5eb6b37e2662e
blocho <blocho@bbcag.ch>
2023-07-05 15:08:21 +0200

    Created the List function

--- app/src/main/java/com/example/solarsearch_project/helper/ElementJsonParser.java
+++ app/src/main/java/com/example/solarsearch_project/helper/ElementJsonParser.java
@@ -1,17 +1,32 @@
 package com.example.solarsearch_project.helper;
 
+import android.util.Log;
+
 import com.example.solarsearch_project.models.Element;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class ElementJsonParser {
- public static Element createElementFromJsonString(String elementJsonString) throws
-         JSONException {
-     Element element = new Element();
-     JSONObject jsonObj = new JSONObject(elementJsonString);
-     element.setId(jsonObj.getString("id"));
+import java.util.ArrayList;
 
-     return element;
+public class ElementJsonParser {
+ public static ArrayList<Element> createElementFromJsonString(String elementJsonString) throws JSONException {
+
+     ArrayList<Element> elements = new ArrayList<>();
+
+     JSONObject jsonObj = new JSONObject(elementJsonString);
+     JSONArray bodies = jsonObj.getJSONArray("bodies");
+     if (bodies != null) {
+         for (int i=0;i<bodies.length();i++){
+             JSONObject jsonElement = new JSONObject(bodies.getString(i));
+
+             Element element = new Element();
+             element.setId(jsonElement.getString("id"));
+             elements.add(element);
+         }
+     }
+
+     return elements;
  }
 }
--- app/src/main/java/com/example/solarsearch_project/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/solarsearch_project/ui/search/SearchFragment.java
@@ -1,6 +1,7 @@
 package com.example.solarsearch_project.ui.search;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,7 @@ import androidx.lifecycle.ViewModelProvider;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.solarsearch_project.databinding.FragmentSearchBinding;
@@ -23,12 +25,13 @@ import com.example.solarsearch_project.models.Element;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchFragment extends Fragment {
 
+    private static final String Solar_API_ID = "https://api.le-systeme-solaire.net/rest.php/bodies?data=id";
     private FragmentSearchBinding binding;
-    private static final String Solar_API_ID = "https://api.le-systeme-solaire.net/rest/bodies/";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -38,14 +41,9 @@ public class SearchFragment extends Fragment {
         binding = FragmentSearchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-
-
-
+        addElementClickableList(Solar_API_ID);
 
         return root;
-
-
-
     }
 
     @Override
@@ -54,32 +52,35 @@ public class SearchFragment extends Fragment {
         binding = null;
     }
 
-   /* private void addElementClickableList(String url) {
+    private void addElementClickableList(String url) {
         ListView listElements = binding.listElements;
         final ArrayAdapter<Element> elementArrayAdapter = new
                 ArrayAdapter<>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1);
+        listElements.setAdapter(elementArrayAdapter);
+
         RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+            new Response.Listener<String>() {
                  @Override
                     public void onResponse(String response) {
 
-                     Element element = null;
                      try {
-                         element = ElementJsonParser.createElementFromJsonString(response);
+                         ArrayList<Element> elements = ElementJsonParser.createElementFromJsonString(response);
+                         elementArrayAdapter.addAll(elements);
                      } catch (JSONException e) {
                          throw new RuntimeException(e);
                      }
-                     elementArrayAdapter.addAll(element.getId());
-                         listElements.setAdapter(elementArrayAdapter);
-
                  }
-                });
-
-
-
-
-    }*/
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Error2BeHandelt", error.toString());
+                }
+            }
+        );
+        queue.add(stringRequest);
+    }
 
 
 }
